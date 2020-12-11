8c3aeec3cc15f066de653ca04ab1d2f6fa283ee0
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-11 04:35:36 +0100

    update

--- src/main/java/dare/spring5recipe/app/spring5recipeapp/domain/Recipe.java
+++ src/main/java/dare/spring5recipe/app/spring5recipeapp/domain/Recipe.java
@@ -119,6 +119,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
